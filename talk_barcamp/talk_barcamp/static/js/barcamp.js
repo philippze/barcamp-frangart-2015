dce261982f530869ef4114d3156e224ba0af34b0
Philipp Zedler <philipp@neue-musik.com>
2015-06-06 00:01:16 +0200

    Mainly finished

--- talk_barcamp/talk_barcamp/static/js/barcamp.js
+++ talk_barcamp/talk_barcamp/static/js/barcamp.js
@@ -23,6 +23,21 @@ $(document).ready(function () {
         $('.twocolumn .col-md-6').css({'max-height': height + 'px'});
     }
     adjust_twocolumn();
+    $(window).load(adjust_twocolumn);
     $(window).resize(adjust_twocolumn);
+    
+    function adjust_iframes () {
+        $('iframe').each(function () {
+            var width = $(this).parent().width();
+            var height = $(window).height() - $(this).offset().top - 40;
+            $(this).attr({
+                width: width,
+                height: height
+            });
+        });
+    }
+    adjust_iframes();
+    $(window).load(adjust_iframes);
+    $(window).resize(adjust_iframes);
     
 });
